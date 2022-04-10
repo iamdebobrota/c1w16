6cd01975da7bc3540501ee7fbcb8aa2bf573ea42
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-04-10 13:18:22 +0530

    first

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,14 +1,28 @@
-
 // keep the add to cart component here
-const CartButton = () => {
+import React,{useState} from "react";
+const CartButton = (props) => {
     //manage state of the count 
+    const [count, setCount] = useState(0);
+
+    const handleInc=() =>{
+      setCount(count+1)
+  };
+const handleDec=()=>{
+  if(count>0){
+    setCount(count-1)
+  }
+  // setCount(count-1)
+}
+
+    
   return <>
   {/* add to cart button */}
   {/* count with - and  + button */}
-  <div>
-      <button>-</button>
+
+  <div className="btn">
+      <button  onClick={handleInc} className="minus-btn">+</button>
       <p className="count-item">{count}</p>
-      <button>+</button>
+      <button  onClick={handleDec}  className="plusbtn">-</button>
   </div>
   </>;
 };
